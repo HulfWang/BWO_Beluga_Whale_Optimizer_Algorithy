5ff2b2a8a943e033756052fe274ec417cd34593a
Hulf Wang <128271422+HulfWang@users.noreply.github.com>
2025-01-11 11:45:41 +0800

    BWO.py

--- BWO.py
+++ BWO.py
@@ -18,10 +18,26 @@
 import random
 import numpy
 import math
-from solution import solution
 import time
 import copy
 
+
+# 定义解的情况
+class solution:
+    def __init__(self):
+        self.best = 0
+        self.bestIndividual = []
+        self.convergence = []
+        self.optimizer = ""
+        self.objfname = ""
+        self.startTime = 0
+        self.endTime = 0
+        self.executionTime = 0
+        self.lb = 0
+        self.ub = 0
+        self.dim = 0
+        self.popnum = 0
+        self.maxiers = 0
 
 # 种群排序 返回排序后的适应度和顺序
 def SortFitness(Fit):
